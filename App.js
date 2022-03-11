6c326e89b8a1f2e0ac6a5b310c99c45ea675ca7b
shige8 <shigeyuki.ynk@gmail.com>
2022-03-11 19:33:47 +0900

    ADD_install_firebse、ログイン、登録画面追加

--- App.js
+++ App.js
@@ -3,17 +3,42 @@ import { StyleSheet, Text, View, FlatList } from "react-native";
 import "react-native-get-random-values";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import UsersScreen from "./screens/UsersScreen";
 import CalendarScreen from "./screens/CalendarScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import LoginScreen from "./screens/LoginScreen";
 import { Ionicons } from "@expo/vector-icons";
+import { auth } from "./src/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+  // const Stack = createNativeStackNavigator();
+  const [user, setUser] = useState('');
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+        setUser(user);
+      } else {
+        setUser('');
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="ユーザー一覧">
+      {/* <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <Stack.Screen name="ログイン" component={LoginScreen} />
+        )}
+      </Stack.Navigator> */}
+      <Tab.Navigator>
         <Tab.Screen
           name="Home"
           component={HomeScreen}
@@ -33,11 +58,29 @@ export default function App() {
           }}
         />
         <Tab.Screen
+          name="ユーザー登録"
+          component={RegisterScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
           name="カレンダー"
           component={CalendarScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <Ionicons name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="ログイン"
+          component={LoginScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="people" size={size} color={color} />
             ),
           }}
         />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import { auth } from "../src/firebase";
+import { signOut } from "firebase/auth";
 
 const HomeScreen = () => {
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("logout");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
     <View style={styles.container}>
       <View style={styles.text}>
         <Text>Welcome to UcmsApp!</Text>
+        <TouchableOpacity
+        onPress={handleLogout}
+        style={{
+          marginTop: 10,
+          padding: 10,
+          backgroundColor: '#88cb7f',
+          borderRadius: 10,
+          width: 100,
+        }}
+      >
+        <Text style={{ color: 'white' }}>ログアウト</Text>
+      </TouchableOpacity>
       </View>
     </View>
   );
