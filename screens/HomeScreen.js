78a34a7840d63060f1911e05051ca7e3058b231c
shige8 <shigeyuki.ynk@gmail.com>
2022-03-21 21:50:16 +0900

    ADD_strftime_user毎の通知可能

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useEffect } from "react";
 import { auth } from "../src/firebase";
-import { signOut, currentUser } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import * as Notifications from "expo-notifications";
 import { db } from "../src/firebase";
-import { getDocs, collection, query, where } from "firebase/firestore/lite";
+import { getDocs, collection } from "firebase/firestore/lite";
 
 const HomeScreen = () => {
   const handleLogout = () => {
@@ -20,32 +20,35 @@ const HomeScreen = () => {
   const user = auth.currentUser;
 
   const usersCollectionRef = collection(db, "users");
+
   getDocs(usersCollectionRef).then((snapshot) => {
     snapshot.docs.map((doc) => {
       if (user.uid === doc.data().uid) {
-        const med = doc.data().taking_medicine_at;
-        console.log(med);
-      }
+        const strftime = require("strftime");
+        // var strftime = require("strftime")
+        const med = doc.data().taking_medicine_at.toDate();
+        const m = strftime("%B %d, %Y %H:%M:%S", new Date(med));
+        // var strftime = require("strftime");
+        const m_hour = Number(strftime("%H", new Date(m)));
+        // var strftime = require("strftime");
+        const m_minute = Number(strftime("%M", new Date(m)));
+        console.log(m);
+        Notifications.scheduleNotificationAsync({
+            content: {
+              body: "服薬を記録して、一緒に習慣化しましょう！",
+              title: "UcmsApp",
+              subtitle: "今日の服薬はおわりましたか？",
+            },
+            trigger: {
+              hour: m_hour,
+              minute: m_minute,
+              repeats: true,
+            },
+          });
+        };
     });
   });
-  // const q = query(collection(db, "users"), where("uid", user.uid));
-
-
-  const scheduleNotificationAsync = async () => {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        body: "服薬を記録して、一緒に習慣化しましょう！",
-        title: "UcmsApp",
-        subtitle: "今日の服薬はおわりましたか？",
-      },
-      trigger: {
-        hour: med,
-        minute: med,
-        repeats: true,
-      },
-    });
-  };
-  
+    
   return (
     <View style={styles.container}>
       <View style={styles.text}>
@@ -62,7 +65,9 @@ const HomeScreen = () => {
           <Text style={{ color: "white" }}>ログアウト</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={scheduleNotificationAsync}
+          onPress={async () => {
+            await Notifications.cancelAllScheduledNotificationsAsync();
+          }}
           style={{
             marginTop: 10,
             padding: 10,
@@ -70,7 +75,7 @@ const HomeScreen = () => {
             borderRadius: 10,
           }}
         >
-          <Text style={{ color: "white" }}>3秒後にプッシュ通知する</Text>
+          <Text style={{ color: "white" }}>通知終了</Text>
         </TouchableOpacity>
       </View>
     </View>
