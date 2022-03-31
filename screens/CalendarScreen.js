0cf4f7d3a4ca42ad6f8264d17f560ca86af3480c
shige8 <shigeyuki.ynk@gmail.com>
2022-03-31 14:55:05 +0900

    ADD_notification

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,21 +1,65 @@
 import { Calendar, LocaleConfig } from "react-native-calendars";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useState, useEffect } from "react";
 import "react-native-get-random-values";
 import { REACT_APP_DEV_API_URL_MEDICINES } from "@env";
-import ListMedicine from "../components/ListMedicine";
+import ListMedicine from "../components/ListMedicine";import {
+  getDocs,
+  getDoc,
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore/lite";
+import { auth } from "../src/firebase";
+import { db } from "../src/firebase";
+import * as Notifications from "expo-notifications";
 
 const CalendarScreen = () => {
-  const [medicines, setMedicines] = useState([]);
+  // const [medicines, setMedicines] = useState([]);
+
+  // useEffect(() => {
+  //   const getMedicine = async () => {
+  //     const res = await fetch(REACT_APP_DEV_API_URL_MEDICINES);
+  //     const medicines = await res.json();
+  //     setMedicines(medicines);
+  //   };
+  //   getMedicine();
+  // }, []);
+
+  const signInUser = auth.currentUser;
+  const userRef = doc(db, "users", `${signInUser.uid}`);
 
   useEffect(() => {
-    const getMedicine = async () => {
-      const res = await fetch(REACT_APP_DEV_API_URL_MEDICINES);
-      const medicines = await res.json();
-      setMedicines(medicines);
-    };
-    getMedicine();
+    getDoc(userRef).then((snapshot) => {
+      const strftime = require("strftime");
+      // var strftime = require("strftime")
+      const med = snapshot.data().taking_medicine_at;
+      console.log(med);
+      const m = strftime("%B %d, %Y %H:%M:%S", new Date(med));
+      // var strftime = require("strftime");
+      const m_hour = Number(strftime("%H", new Date(m)));
+      // var strftime = require("strftime");
+      const m_minute = Number(strftime("%M", new Date(m)));
+      console.log(m_minute);
+      Notifications.cancelAllScheduledNotificationsAsync();
+      Notifications.scheduleNotificationAsync({
+        content: {
+          body: "服薬を記録して、一緒に習慣化しましょう！",
+          title: "UcmsApp",
+          subtitle: "今日の服薬はおわりましたか？",
+        },
+        trigger: {
+          hour: m_hour,
+          minute: m_minute,
+          repeats: true,
+        },
+      });
+    });
   }, []);
+
   return (
     <View style={styles.container}>
       <Calendar
@@ -24,7 +68,7 @@ const CalendarScreen = () => {
           "2022-02-16": { selected: true, selectedColor: "lightgreen" },
         }}
       />
-      <FlatList
+      {/* <FlatList
         data={ medicines }
         renderItem={({ item }) => (
           <ListMedicine item={item} />
@@ -38,7 +82,29 @@ const CalendarScreen = () => {
             }}
           ></View>
         )}
-      />
+      /> */}
+      <TouchableOpacity
+        onPress={async () => {
+          try {
+            const notifications =
+              await Notifications.getAllScheduledNotificationsAsync();
+            // const identifier = notifications[0].identifier;
+            // console.log(identifier);
+            console.log(notifications);
+            // await Notifications.cancelAllScheduledNotificationsAsync()
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+        }}
+        style={{
+          marginTop: 10,
+          padding: 10,
+          backgroundColor: "#88cb7f",
+          borderRadius: 10,
+        }}
+      >
+        <Text style={{ color: "white" }}>通知終了</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { auth } from "../src/firebase";
 import { signOut } from "firebase/auth";
 import * as Notifications from "expo-notifications";
 import { db } from "../src/firebase";
-import { getDocs, collection } from "firebase/firestore/lite";
+import {
+  getDocs,
+  getDoc,
+  collection,
+  addDoc,
+  doc,
+  updateDoc,
+  setDoc,
+  Timestamp,
+} from "firebase/firestore/lite";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { useNavigation } from "@react-navigation/native";
+
 
 const HomeScreen = () => {
   const handleLogout = () => {
@@ -18,41 +30,120 @@ const HomeScreen = () => {
   };
 
   const user = auth.currentUser;
+  const userRef = doc(db, "users", `${user.uid}`);
 
-  const usersCollectionRef = collection(db, "users");
-
-  getDocs(usersCollectionRef).then((snapshot) => {
-    snapshot.docs.map((doc) => {
-      if (user.uid === doc.data().uid) {
-        const strftime = require("strftime");
-        // var strftime = require("strftime")
-        const med = doc.data().taking_medicine_at.toDate();
-        const m = strftime("%B %d, %Y %H:%M:%S", new Date(med));
-        // var strftime = require("strftime");
-        const m_hour = Number(strftime("%H", new Date(m)));
-        // var strftime = require("strftime");
-        const m_minute = Number(strftime("%M", new Date(m)));
-        console.log(m);
-        Notifications.scheduleNotificationAsync({
-            content: {
-              body: "服薬を記録して、一緒に習慣化しましょう！",
-              title: "UcmsApp",
-              subtitle: "今日の服薬はおわりましたか？",
-            },
-            trigger: {
-              hour: m_hour,
-              minute: m_minute,
-              repeats: true,
-            },
-          });
-        };
+  useEffect(() => {
+    getDoc(userRef).then((snapshot) => {
+      const strftime = require("strftime");
+      // var strftime = require("strftime")
+      const med = snapshot.data().taking_medicine_at;
+      console.log(med);
+      const m = strftime("%B %d, %Y %H:%M:%S", new Date(med));
+      // var strftime = require("strftime");
+      const m_hour = Number(strftime("%H", new Date(m)));
+      // var strftime = require("strftime");
+      const m_minute = Number(strftime("%M", new Date(m)));
+      console.log(m_minute);
+      Notifications.cancelAllScheduledNotificationsAsync();
+      Notifications.scheduleNotificationAsync({
+        content: {
+          body: "服薬を記録して、一緒に習慣化しましょう！",
+          title: "UcmsApp",
+          subtitle: "今日の服薬はおわりましたか？",
+        },
+        trigger: {
+          hour: m_hour,
+          minute: m_minute,
+          repeats: true,
+        },
+      });
     });
-  });
-    
+  }, []);
+
+  const [notification, setNotification] = useState("");
+
+  // const onChangeNotification = async (event) => {
+  // event.preventDefault();
+  // const { date } = event.target.elements;
+
+  //   setNotification(event.target.value);
+  // };
+
+  const setDate = (event, date) => {
+    // event.preventDefault();
+    setNotification(date);
+  };
+  const navigation = useNavigation();
+  
+  const onClickAdd = async () => {
+    // const ref = db.collection(`users/${user.uid}/taking_medicine_at`);
+    // ref.addDoc({
+    //   taking_medicine_at: notification,
+    // })
+    // .then((docRef) => {
+    //   console.log(docRef.id);
+    // })
+    // .catch((error) => {
+    //   console.log(error);
+    // })
+    try {
+      //  getDocs(usersCollectionRef).then((snapshot) => {
+      //    snapshot.docs.map((doc) => {
+      //      if (user.uid === doc.data().uid) {
+      //        const docId = doc.id;
+      //      }
+      //    });
+      //  });
+      await setDoc(doc(db, "users", `${user.uid}`), {
+        taking_medicine_at: `${notification}`,
+      });
+      console.log(notification);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    navigation.navigate("Calendar")
+  };
+
+  
   return (
     <View style={styles.container}>
+      <Text style={{ fontSize: 30, marginBottom: 50 }}>UcmsAppにようこそ</Text>
+      <Text style={{ fontSize: 30, marginTop: 10, marginBottom: 10 }}>
+        毎日の服薬記録で習慣化
+      </Text>
+      {/* <Text style={{ fontSize: 24, marginTop: 30 }}>
+        服薬を忘れないよう通知します
+      </Text> */}
+      {/* <Text style={{ fontSize: 16, top: 10, margin: 20 }}>
+        通知の時間を選んでください
+      </Text> */}
+      <RNDateTimePicker
+        onChange={setDate}
+        style={{ width: 200 }}
+        value={notification || new Date()}
+        mode="time"
+        // is24Hour={true}
+        display="spinner"
+        minimumDate={new Date()}
+        // minuteInterval="10"
+        // textColor="red"
+        // neutralButtonLabel="clear"
+      />
+      <TouchableOpacity
+        onPress={onClickAdd}
+        style={{
+          marginTop: 10,
+          padding: 20,
+          backgroundColor: "#88cb7f",
+          borderRadius: 10,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>
+          この時刻に通知をうけとる
+        </Text>
+      </TouchableOpacity>
       <View style={styles.text}>
-        <Text>Welcome to UcmsApp!</Text>
+        {/* <Text>Welcome to UcmsApp!</Text> */}
         <TouchableOpacity
           onPress={handleLogout}
           style={{
@@ -66,7 +157,16 @@ const HomeScreen = () => {
         </TouchableOpacity>
         <TouchableOpacity
           onPress={async () => {
-            await Notifications.cancelAllScheduledNotificationsAsync();
+            try {
+              const notifications =
+                await Notifications.getAllScheduledNotificationsAsync();
+              // const identifier = notifications[0].identifier;
+              // console.log(identifier);
+              console.log(notifications);
+              // await Notifications.cancelAllScheduledNotificationsAsync()
+            } catch (e) {
+              console.error("Error adding document: ", e);
+            }
           }}
           style={{
             marginTop: 10,
@@ -80,7 +180,7 @@ const HomeScreen = () => {
       </View>
     </View>
   );
-};
+};;
 
 const styles = StyleSheet.create({
   container: {
